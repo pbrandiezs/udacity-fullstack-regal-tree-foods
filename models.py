6da8968f89991961c0684b01dd795622597b0a31
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 12:31:02 -0700

    fix: Changed fruit_tester.py to use LF

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
 import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 
