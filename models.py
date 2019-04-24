2acaf53d02da6c616293e186fa7719eac187765c
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 10:59:41 -0700

    feat: Add generate and verify auth token

--- models.py
+++ models.py
@@ -24,9 +24,25 @@ class User(Base):
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
-    #Add a method to generate auth tokens here
     
-    #Add a method to verify auth tokens here
+	def generate_auth_token(self, expiration=600):
+    	s = Serializer(secret_key, expires_in = expiration)
+    	return s.dumps({'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+    	s = Serializer(secret_key)
+    	try:
+    		data = s.loads(token)
+    	except SignatureExpired:
+    		#Valid Token, but expired
+    		return None
+    	except BadSignature:
+    		#Invalid Token
+    		return None
+    	user_id = data['id']
+    	return user_id
+
 
 class Product(Base):
     __tablename__ = 'product'
