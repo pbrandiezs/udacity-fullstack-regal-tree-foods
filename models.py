5ed0eed66006da64293094f16c663bb47a5196a8
Perry Brandiezs <pbrandiezs@gmail.com>
2019-04-24 10:53:40 -0700

    feat: Added starter code to models.py

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from sqlalchemy import Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
@@ -9,6 +7,8 @@ import random, string
 from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
+
+#You will use this secret key to create and verify your tokens
 secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
 class User(Base):
@@ -18,31 +18,30 @@ class User(Base):
     password_hash = Column(String(64))
 
     def hash_password(self, password):
-        self.password_hash = pwd_context.hash(password)
+        self.password_hash = pwd_context.encrypt(password)
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+    #Add a method to generate auth tokens here
+    
+    #Add a method to verify auth tokens here
 
-    def generate_auth_token(self, expiration=600):
-    	s = Serializer(secret_key, expires_in = expiration)
-    	return s.dumps({'id': self.id })
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    category = Column(String)
+    price = Column(String)
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+        'name' : self.name,
+        'category' : self.category,
+        'price' : self.price
+            }
 
-    @staticmethod
-    def verify_auth_token(token):
-    	s = Serializer(secret_key)
-    	try:
-    		data = s.loads(token)
-    	except SignatureExpired:
-    		#Valid Token, but expired
-    		return None
-    	except BadSignature:
-    		#Invalid Token
-    		return None
-    	user_id = data['id']
-    	return user_id
-
-
-engine = create_engine('sqlite:///usersWithTokens.db')
+engine = create_engine('sqlite:///regalTree.db')
  
 
 Base.metadata.create_all(engine)
